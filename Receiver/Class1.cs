db392d8e7c05a569572ade7ee0b50e697774c541
Ramon Smits <ramon.smits@gmail.com>
2020-04-28 18:11:41 +0200

    Writing machine + process id and removed timestamp as already logged by NLog.

--- Receiver/Class1.cs
+++ Receiver/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Globalization;
 using System.Threading.Tasks;
 using NServiceBus;
@@ -6,7 +7,8 @@ using NServiceBus.Logging;
 
 public class UpdateOrderHandler : IHandleMessages<MyMessage>
 {
-    private readonly ILog _logger = LogManager.GetLogger<UpdateOrderHandler>();
+    static readonly int ProcessId = Process.GetCurrentProcess().Id;
+    readonly ILog _logger = LogManager.GetLogger<UpdateOrderHandler>();
 
     public Task Handle(MyMessage message, IMessageHandlerContext context)
     {
@@ -15,19 +17,7 @@ public class UpdateOrderHandler : IHandleMessages<MyMessage>
         var messageId = string.Empty;
         context?.MessageHeaders.TryGetValue(Headers.DelayedRetries, out numberDelayedRetries);
         context?.MessageHeaders.TryGetValue(Headers.MessageId, out messageId);
-        return UpdateOrderAsync(message, $"delayed retry count '{numberDelayedRetries ?? "0"}', message id '{messageId ?? string.Empty}, Timestamp: {DateTime.UtcNow.ToString("HH:mm:ss.fff", CultureInfo.InvariantCulture)}'");
-    }
-
-    private Task UpdateOrderAsync(MyMessage message, string nsbMessageInfo)
-    {
-        try
-        {
-            throw new Exception("I intentionally fail all attempts");
-        }
-        catch
-        {
-            _logger.Warn(nsbMessageInfo);
-            throw;
-        }
+        _logger.Warn($"Process '{ProcessId}', Machine '{Environment.MachineName}', delayed retry count '{numberDelayedRetries ?? "0"}', message id '{messageId ?? string.Empty}");
+        throw new Exception("I intentionally fail all attempts");
     }
 }
